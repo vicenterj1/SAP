01a4b0493cf24521ab7c31c49e5ac53a419e7ebc
vicenterj1 <vicenterj1@gmail.com>
2024-08-19 16:22:31 +0000

    aprovacao de solicitacao

---
+++ aprovacao de solicitacoes/aprovacaosolicitacao/webapp/controller/NotFound.controller.js
@@ -0,0 +1,19 @@
+
+sap.ui.define([
+    "./BaseController"
+], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("vicente.aprovacaosolicitacao.aprovacaosolicitacao.controller.NotFound", {
+
+        /**
+         * Navigates to the worklist when the link is pressed
+         * @public
+         */
+        onLinkPressed : function () {
+            this.getRouter().navTo("worklist");
+        }
+
+    });
+
+});
---
+++ aprovacao de solicitacoes/aprovacaosolicitacao/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"vicenteaprovacaosolicitacao/aprovacaosolicitacao/test/unit/controller/Worklist.controller"
+], function () {
+	"use strict";
+});
